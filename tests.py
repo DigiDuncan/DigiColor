191c8c502a589bb8d09fc6998d7775ac648f765a
DigiDuncan <digiduncan@gmail.com>
2020-03-29 21:27:51 -0400

    Make the test file formatted nicely.

--- tests.py
+++ tests.py
@@ -2,4 +2,7 @@ import doctest
 
 from digicolor.lib import colors
 
-doctest.testmod(colors, extraglobs={"mycolor": colors.colors.add(colorid = 256, name = "AWESOME", value = 0x0fcdf7)})
+doctest.testmod(colors, extraglobs=
+                {"mycolor": colors.colors.add(colorid = 256,
+                                              name = "AWESOME",
+                                              value = 0x0fcdf7)})
